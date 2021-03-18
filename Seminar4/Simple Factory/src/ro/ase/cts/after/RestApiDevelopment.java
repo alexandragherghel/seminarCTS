3f1be4e3b4f776778e3582cbf0a00a89f4654334
alexandragherghel <72292156+alexandragherghel@users.noreply.github.com>
2021-03-18 19:26:34 +0200

    Design patterns creationale

---
+++ Seminar4/Simple Factory/src/ro/ase/cts/after/RestApiDevelopment.java
@@ -0,0 +1,13 @@
+package ro.ase.cts.after;
+
+import ro.ase.cts.after.contracts.RestApi;
+
+public class RestApiDevelopment implements RestApi {
+    public RestApiDevelopment() {
+        System.out.println("ApiDevelopment");
+    }
+    @Override
+    public void connect() {
+        System.out.println("Connected successfully");
+    }
+}
---
+++ Seminar4/Simple Factory/src/ro/ase/cts/before/RestApiRelease.java
@@ -0,0 +1,11 @@
+package ro.ase.cts.before;
+
+public class RestApiRelease {
+    public RestApiRelease() {
+        System.out.println("ApiRelease");
+    }
+
+    public void connect() {
+        System.out.println("Connected successfully");
+    }
+}
